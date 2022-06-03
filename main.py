68fe7a3629fd7ee124796c0d7092c40ff2327f9b
Tiew <tnttiew@gmail.com>
2022-06-03 17:57:30 +0700

    cmit

---
+++ main.py
@@ -0,0 +1,6 @@
+import mysum
+
+
+list_to_sum = [3,5,7]
+
+mysum.my_sum(list_to_sum)
---
+++ mysum.py
@@ -0,0 +1,8 @@
+def my_sum(iter):
+    for num in iter:
+        print(num)
+    print('sum-------')
+    print(sum(iter))
+    print('----------')
+
+my_sum([4,6,5,3])
